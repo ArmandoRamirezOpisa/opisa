4b47de61957c94d4ff4c9e90956d32abe9ea7087
Armando Ramirez <armando.ramirez@opisa.com>
2020-06-03 09:02:15 -0500

    se esta analizando la pagina de opisa

--- opisaFramework/application/views/nosotros_view.php
+++ opisaFramework/application/views/nosotros_view.php
@@ -11,6 +11,14 @@
     <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="../assets/lib/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/main.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168470096-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-168470096-1');
+        </script>
     <title>OPI</title>
 </head>
 
